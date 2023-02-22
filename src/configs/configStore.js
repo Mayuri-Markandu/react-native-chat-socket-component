20439ab6c9a5cdd1e895d5f96f8e0304d8538b9e
silambarasan6499 <silammca@gmail.com>
2023-02-22 07:01:17 +0000

    Locofy commit

--- src/configs/configStore.js
+++
@@ -1,7 +0,0 @@
-//Configure Store
-
-import { createStore, applyMiddleware } from "redux";
-import { rootReducer } from "reducers";
-import thunk from "redux-thunk";
-
-export const configStore = createStore(rootReducer, applyMiddleware(thunk));
--- src/lib/networking/Types.js
+++
@@ -1,8 +0,0 @@
-
-export const LOGIN_SUCCESS = "login_success";
-export const LOGIN_FAILURE = "login_failure";
-export const USERID = 'user_id';
-export const ROOMDETAILS = 'room';
-
-
-
